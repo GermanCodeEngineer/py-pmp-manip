806171cccf134ffa21ac7ad8c30342560dfaa56c
GermanCodeEngineer <GermanCodeEngineer@proton.me>
2025-09-20 21:53:47 +0200

    updated install_test

--- install_test/docker_test.py
+++ install_test/docker_test.py
@@ -1,6 +1,8 @@
+# Test Importing and Project Creation
 from pmp_manip import SRProject
 SRProject.create_empty()
 
+# Test Building an extension
 from pmp_manip import (
     get_default_config, init_config,
     info_api,
@@ -9,6 +11,12 @@ from pmp_manip import (
 cfg = get_default_config()
 init_config(cfg)
 
-info_api.generate_and_add_extension("pmSensingExpansion", extension_source=None)
+extension = "pmSensingExpansion"
+info_api.generate_and_add_extension(extension, extension_source=None)
+found = False
+for opcode in info_api.opcode_info.keys_key1():
+    if opcode.startswith(f"{extension}_"):
+        found = True
+assert found, "Did not add extension successfully"
 
 print("Package imported successfully!")
